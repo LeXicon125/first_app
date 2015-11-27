91fa1157d88e01d632ce22a51e80231ffc554b4f
LeXicon <lexicon125@gmail.com>
2015-11-27 10:54:31 -0800

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '73ca8012cb743da60e4ecabb0ea64d89adcca4d01856e3427f13e01e86343246e67bfcb9f847c7ce4bd72526ea5870e7439a6d509a3b99babd8edf5acdc18e74'
